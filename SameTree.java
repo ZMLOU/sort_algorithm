ea57b406e2db6152c19115c595b7428779cee603
2280718088 <2280718088@qq.com>
2019-06-02 16:46:32 +0800

    相同的树

---
+++ SameTree.java
@@ -0,0 +1,20 @@
+package com.tech.binTree;
+
+public class SameTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null){
+            return true;
+        }
+        if(p != null && q != null && p.val == q.val){
+            return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        }
+        return false;
+    }
+}
